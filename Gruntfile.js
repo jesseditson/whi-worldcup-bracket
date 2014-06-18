2dc8a5cb0791ccc8c94db52b5094f082601d2f0c
Jesse Ditson <jesse.ditson@gmail.com>
2014-06-18 13:10:57 -0700

    include scoreUpdater in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,8 @@ module.exports = function (grunt) {
             '.htaccess',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
-            'fonts/**/*'
+            'fonts/**/*',
+            '../scoreUpdater/**/*'
           ]
         }, {
           expand: true,
